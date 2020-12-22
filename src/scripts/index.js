f24785d3c218829ba84bb57e4048ff59de2f2296
Dyuk <Dyuksik@icloud.com>
2020-12-22 21:59:58 +0300

    add html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,3 +5,42 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 console.log('webpack starterkit');
+
+
+let column = [
+  {id: 1, name: "Todo"},
+  {id: 2, name: "First"},
+  {id: 3, name: "Second"}
+];
+
+let card = [
+  {id: 1, description: "qweqwr"},
+  {id: 2, description: "qweasdasfqwr"},
+  {id: 3, description: "qwevdsaqwr"}
+];
+ let users = [
+   {id: 1, name: "Gena", img: "src"},
+   {id: 2, name: "Vova", img: "src"},
+   {id: 3, name: "Potap", img: "src"}
+ ];
+
+ 
+ 
+ function addColumn(column) {
+   let addCol = document.querySelector('.wrapper'); 
+   addCol.innerHTML += `<div class="column" id:${column.id}>${card.description}</div>`;
+  }
+
+  function addCard(column) {
+    let addCards = document.querySelector('.column'); 
+    addCards.innerHTML += `<div class="card" id:${card.id}>${column.name}</div>`;
+   }
+  
+  for (let i = 0; i < column.length; i++) {
+    debugger;
+    addColumn(column[i]); 
+ }
+
+ for (let i = 0; i < card.length; i++) {
+  addCard(card[i]); 
+}
