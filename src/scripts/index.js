0d5986fdc0aed4f1e9517fce1bc7a33180c0511a
Dyuk <Dyuksik@icloud.com>
2020-12-24 19:21:04 +0300

    add value

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,43 +8,41 @@ if (process.env.NODE_ENV === 'development') {
 
 
 let column = [
-  {id: 1, name: "Todo"},
-  {id: 2, name: "First"},
-  {id: 3, name: "Second"}
-];
+  {id: 1, name: "Todo", order: 1},
+  {id: 2, name: "First", order: 2},
+  {id: 3, name: "Second", order: 3}
+]
 
 let card = [
-  {id: 1, description: "qweqwr"},
-  {id: 2, description: "qweasdasfqwr"},
-  {id: 3, description: "qwevdsaqwr"}
+  { id: 1, description: "qweqwr", value: 1 },
+  { id: 2, description: "qweasdasfqwr", value: 2 },
+  { id: 3, description: "qwevdsaqwr", value: 3 }
 ];
- let users = [
-   {id: 1, name: "Gena", img: "src"},
-   {id: 2, name: "Vova", img: "src"},
-   {id: 3, name: "Potap", img: "src"}
- ];
+let users = [
+  { id: 1, name: "Gena", img: "src" },
+  { id: 2, name: "Vova", img: "src" },
+  { id: 3, name: "Potap", img: "src" }
+];
 
- 
- 
- function addColumn(column) {
-   let addCol = document.querySelector('.wrapper'); 
-   addCol.innerHTML += `<div class="column" id:${column.id}>${card.description}</div>`;
-  }
 
-  function addCard(column) {
-    let addCards = document.querySelector('.column'); 
-    if (column.id == card.id) {
+
+function addColumn(column) {
+  let addCol = document.querySelector('.wrapper');
+  addCol.innerHTML += `<div class="column" id:${column.id}>${card.description}</div>`;
+}
+
+function addCard(column) {
+  let addCards = document.querySelector('.column');
     addCards.innerHTML += `<div class="card" id:${card.id}>${column.name}</div>`;
-    }
-   }
-  
-  for (let i = 0; i < column.length; i++) {
-    debugger;
-    addColumn(column[i], card[i]); 
-    for (let i = 0; i < card.length; i++) {
-      if (column[i].id == card[i].id) {
+}
+
+for (let i = 0; i < column.length; i++) {
+  debugger;
+  addColumn(column[i], card[i]);
+  for (let i = 0; i < card.length; i++) {
+    if (column[i].order === card[i].value) {
       addCard(card[i]);
-      } 
     }
- }
+  }
+}
 
